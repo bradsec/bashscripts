74a7cea907780fc0f6ea3bdbf656ac4c265585c7
Mark <mark@lendeb.network.local>
2022-07-16 13:33:33 +1000

    Add additional collabapps.sh applications

--- collabapps.sh
+++ collabapps.sh
@@ -42,6 +42,37 @@ install_discord() {
 	pkgman fix
 }
 
+install_teams() {
+	message INFO "Installing Microsoft Teams..."
+	from_url="https://go.microsoft.com/fwlink/p/?LinkID=2112886&clcid=0x409&culture=en-us&country=US"
+	save_file="/tmp/teams.deb"
+	download_file ${save_file} ${from_url}
+	pkgman install ${save_file}
+	pkgman fix
+}
+
+install_slack() {
+	message INFO "Installing Slack..."
+	# Two fixes below for missing packages in Debian Bullseye
+	message INFO "Installing requiried Debian packages..."
+	from_url="http://ftp.mx.debian.org/debian/pool/main/libi/libindicator/libindicator3-7_0.5.0-3+b1_amd64.deb"
+	save_file="/tmp/libind.deb"
+	download_file ${save_file} ${from_url}
+	pkgman install ${save_file}
+	from_url="http://ftp.mx.debian.org/debian/pool/main/liba/libappindicator/libappindicator3-1_0.4.92-7_amd64.deb"
+	save_file="/tmp/libapp.deb"
+	download_file ${save_file} ${from_url}
+	pkgman install ${save_file}
+	# Fetch latest Slack version
+	message INFO "Fetching latest Slack .deb package..."
+	slack_version="$(curl -s https://slack.com/downloads/linux | grep -o '\Version\+ [0-9].2[0-9]....' | awk '{print $2}')"
+	from_url="https://downloads.slack-edge.com/releases/linux/${slack_version}/prod/x64/slack-desktop-${slack_version}-amd64.deb"
+	save_file="/tmp/slack.deb"
+	download_file ${save_file} ${from_url}
+	pkgman install ${save_file}
+	pkgman fix
+}
+
 # Display a list of menu items for selection
 display_menu () {
 	echo
@@ -50,9 +81,13 @@ display_menu () {
     echo -e "=============="
 	echo
     echo -e "1. Install Discord"
-    echo -e "2. Remove Discord"
-    echo -e "3. Exit\n"
-    echo -n "   Enter option [1-3]: "
+	echo -e "2. Install Slack"
+	echo -e "3. Install Microsoft Teams\n"
+    echo -e "4. Remove Discord"
+    echo -e "5. Remove Slack"
+	echo -e "6. Remove Microsoft Teams\n"
+    echo -e "7. Exit\n"
+    echo -n "   Enter option [1-7]: "
 
     while :
     do
@@ -63,9 +98,23 @@ display_menu () {
 			install_discord
 			;;
 		2)  clear
-			pkgman remove discord
+			pkgman remove slack-desktop
+			install_slack
 			;;
 		3)  clear
+			pkgman remove teams
+			install_teams
+			;;
+		4)  clear
+			pkgman remove discord
+			;;
+		5)  clear
+			pkgman remove slack-desktop
+			;;
+		6)  clear
+			pkgman remove teams
+			;;
+		7)  clear
 			exit
 			;;
 		*)  clear
